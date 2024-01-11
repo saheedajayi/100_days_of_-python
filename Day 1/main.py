021774d18e3b1b8fa53e834458e7c3772d76e038
Saheed <saheed.abd.ajayi@gmail.com>
2024-01-11 15:20:19 +0100

    Day 1 of 100 days of python

---
+++ Day 1/main.py
@@ -0,0 +1,13 @@
+print("Saheed")
+print("January 11 2024")
+print("""
+    I am signing up for Replit's 100 days of python
+    challenge!
+    I will make sure to spend some time everyday coding
+    along, for a minimum of 10 minutes a day.
+    I'll be using Replit, an amazing online IDE so I can do
+    this from my phone wherever I happen to be. No excuses
+    for not coding from the middle of a field! 
+""")
+print("I am feeling 😁")
+print("You can follow my progress at replit.com/@saheedajayi13")
