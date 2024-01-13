6b7ec73110dd3e18feda90d1c38fae9347d7b402
Saheed <saheed.abd.ajayi@gmail.com>
2024-01-13 20:57:57 +0100

    concatenation

---
+++ day_3/concatenation.py
@@ -0,0 +1,9 @@
+food_name = input("Name a food > ")
+plant_type = input("Name a plant type > ")
+cooking_method = input("Name a method of cooking > ")
+burned_food = input("What word describes burned food > ")
+item = input("Name a DIY item > ")
+
+print()
+print("MENU")
+print(cooking_method, food_name, "with", burned_food, plant_type, "on a bed of", item)
