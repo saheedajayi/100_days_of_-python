f28a4e7a72ed11ad6fba3f491d83d3f72b4e8e28
Saheed <saheed.abd.ajayi@gmail.com>
2024-01-16 16:09:07 +0100

    day 4 practice day

---
+++ day_4/main.py
@@ -0,0 +1,15 @@
+print("""
+Welcome to your adventure simulator. 
+I am going to ask you a bunch of questions 
+and then create an epic story with you as the star!
+""")
+print()
+name = input("What is your name? ")
+enemy_name = input("What is your worst enemy name? ")
+superpower = input("What is your superpower? ")
+area = input("Where do you live? ")
+food = input("What is your favorite food? ")
+print()
+print("Hello", name+"!", "Your ability to", superpower, "will make sure you never have to look at", enemy_name, "again.")
+print("Go eat", food, "as you walk down the streets of", area, "and use", superpower, "for good and not evil!")
+
